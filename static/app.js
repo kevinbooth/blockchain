ff1a6ebf9df7c94f77d3a2388af753df18da416d
Kevin Booth <kbooth@gykantler.com>
2019-12-03 10:48:13 -0500

    Build out frontend

--- static/app.js
+++ static/app.js
@@ -8,12 +8,19 @@
  *
  ******************************************************************************/
 
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const {Blockchain, Transaction} = require('savjeecoin');
+
 
 /**
  * UNH
  * @namespace
  */
-var UNH = window.UNH || {};
+var UNH = window.UNH || {
+    key: '',
+    chain: ''
+};
 
 /**
  * Checks if DOM is ready for JS to fire
@@ -33,7 +40,61 @@ UNH.ready = function (_fn) {
  * @function
  */
 UNH.init = function () {
+    const btnGenerate = document.getElementsByClassName('ui-generate')[0];
+    const btnAdd = document.getElementsByClassName('ui-add-transaction')[0];
 
+    btnGenerate.addEventListener('click', UNH.generateToKey, false)
+    btnAdd.addEventListener('click', UNH.addTransaction, false)
+
+    UNH.setup();
+};
+
+/**
+ * Fires off all functions on page load
+ * @function
+ */
+UNH.generateToKey = function () {
+    const toAddr = document.getElementsByClassName('ui-to-addr')[0];
+
+    toAddr.value = ec.genKeyPair().getPublic('hex');
+};
+
+/**
+ * Fires off all functions on page load
+ * @function
+ */
+UNH.setup = function () {
+    const fromAddr = document.getElementsByClassName('ui-from-addr')[0];
+
+    UNH.key = ec.genKeyPair();
+    console.log('Public key:', UNH.key.getPublic('hex'));
+    console.log('Private key:', UNH.key.getPrivate('hex'));
+    fromAddr.innerHTML = UNH.key.getPublic('hex');
+
+    UNH.chain = new Blockchain();
+    console.log(UNH.chain);
+};
+
+/**
+ * Fires off all functions on page load
+ * @function
+ */
+UNH.addTransaction = function () {
+    const toAddr = document.getElementsByClassName('ui-to-addr')[0];
+    const amount = parseInt(document.getElementsByClassName('ui-amount')[0]);
+
+    const tx = new Transaction(UNH.key.getPublic('hex'), toAddr, amount);
+    tx.signTransaction(UNH.key);
+
+    UNH.chain.addTransaction(tx);
+};
+
+/**
+ * Fires off all functions on page load
+ * @function
+ */
+UNH.createBlock = function () {
+    
 };
 
 /**
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './static/app.js',
+  output: {
+    filename: 'app.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
